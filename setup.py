612ccea5edeb1b268bd149f280a25c3a1fe25ed6
Prathamesh <prath.parab11@gmail.com>
2024-01-07 18:04:10 +0530

    data transformation added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Machine-Learning-Project"
-AUTHOR_USER_NAME = "prathamesh-parab11"
-SRC_REPO = "mlproject"
-AUTHOR_EMAIL = "prath.parab11@gmail.com"
+REPO_NAME = "End-to-end-ML-Project-with-MLflow"
+AUTHOR_USER_NAME = "entbappy"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
 
 
 setuptools.setup(
